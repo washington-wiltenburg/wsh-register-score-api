5f6a98768716bcd1fad8c037af50522fadf5bc41
Washington Wiltenburg <wsh.cezar@gmail.com>
2020-05-22 10:05:20 -0300

    Initial Commit

---
+++ src/main/java/com/wsh/register/score/api/WshRegisterScoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.wsh.register.score.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WshRegisterScoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WshRegisterScoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wsh/register/score/api/WshRegisterScoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsh.register.score.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WshRegisterScoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
